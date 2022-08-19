ab316cdc15677c909fef6125669229724223a284
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-18 22:23:25 -0300

    18/08/2022

---
+++ Problems/2006.py
@@ -0,0 +1,6 @@
+r = int(input())
+a, b, c, d, e = input().split()
+
+l = [int(a), int(b), int(c), int(d), int(e)]
+
+print(l.count(r))
---
+++ Problems/2374.py
@@ -0,0 +1,5 @@
+n = int(input())
+m = int(input())
+
+res = n - m
+print(res)
